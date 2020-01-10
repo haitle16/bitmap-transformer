133d6ceff6e39232de6525b2360ad4af3d9fbbc2
Papa <martipap@8c8590210f1e.ant.amazon.com>
2020-01-10 13:43:55 -0800

    stopping point

--- src/main/java/bitmap/transformer/Bitmap.java
+++ src/main/java/bitmap/transformer/Bitmap.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class Bitmap {
     File inputFilePath;
-    File outputFilePath;
+    String outputFilePath;
     String transformName;
 
     public Bitmap() throws IOException {
@@ -17,14 +17,12 @@ public class Bitmap {
 //    File bmpFile = new File("bmpImage.bmp");
 //    BufferedImage image = ImageIO.read(bmpFile);
 
-    public Bitmap(File inputFilePath, File outFilePath, String transformName) throws IOException {
+    public Bitmap(File inputFilePath, String outFilePath, String transformName) throws IOException {
         this.inputFilePath = inputFilePath;
         this.outputFilePath = outFilePath;
         this.transformName = transformName;
     }
 
-    public changeWidth(int width) {
-    }
 
 
 }
